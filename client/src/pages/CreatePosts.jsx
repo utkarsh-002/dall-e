2169915cc35320b544ca615f9a1d690e74d24a92
utkarsh <utkarsh.5820@gmail.com>
2023-03-15 22:15:47 +0530

    second commit

--- client/src/pages/CreatePosts.jsx
+++ client/src/pages/CreatePosts.jsx
@@ -23,7 +23,7 @@ const CreatePosts = () =>{
         if(form.prompt && form.photo){
             setLoading(true);
             try {
-                const response= await fetch('http://localhost:8080/api/v1/post',{
+                const response= await fetch('https://creatimity.onrender.com/api/v1/post',{
                 method: 'POST',
                 headers:{
                     'Content-Type': 'application/json',
@@ -53,7 +53,7 @@ const CreatePosts = () =>{
         if(form.prompt){
             try {
                 setGeneratingImg(true);
-                const response = await fetch('http://localhost:8080/api/v1/dalle',{
+                const response = await fetch('https://creatimity.onrender.com/api/v1/dalle',{
                     method: 'POST',
                     headers:{
                         'Content-Type': 'application/json',
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -13,7 +13,7 @@ const Home = () =>{
             setLoading(true);
             
             try {
-                const response = await fetch('http://localhost:8080/api/v1/post',{
+                const response = await fetch('https://creatimity.onrender.com/api/v1/post',{
                 method:'GET',
                 headers:{
                     'Content-Type': 'application/json',
